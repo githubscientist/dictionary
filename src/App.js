3bfeb26f88866bbf395f12d0680c735b1f9f7c83
githubscientist <sathish.datascientist@gmail.com>
2023-04-29 16:20:55 +0530

    added an unordered list

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ function App() {
       <h1>Dictionary App</h1>
       <p>Tool for anyone who wants to quickly look up the
         meaning of a word or improve their vocabulary in a convenient
-      and efficient way.</p>
+        and efficient way.</p>
+      <ul>
+        <li><a>synonyms</a></li>
+        <li><a>antonyms</a></li>
+      </ul>
     </>
   );
 }
